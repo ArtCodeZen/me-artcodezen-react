b74dec4ed440bbfea9d955c7045c14c00496170b
artcodezen <henrique.watpequim@gmail.com>
2022-09-19 21:38:40 -0300

    useEffect adicionado

--- my-app/src/pages/Home/index.js
+++ my-app/src/pages/Home/index.js
@@ -1,40 +1,48 @@
 import "./home.css"
 import imageProfile from "../../assets/img/mePhoto.jpg";
 import imageNote from "../../assets/image.jpg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function Home() {
 
 
-    const STATES =
-    {
-        Programacao: "Programação",
-        Arte: "Arte",
-        Hobby: "Hobby"
-    }
-    const myTitles = [STATES.Programacao, STATES.Arte, STATES.Hobby];
+
+    const [myTitles, setTitles] = useState([]);
     const [infoText, setInfoText] = useState("");
-    function showInfo(stateItem) {
-        switch (stateItem) {
-            case STATES.Programacao:
-                return (
-                    <div>
-                        <h1>{stateItem}</h1>
+    const [myTitle, setMyTitle] = useState("");
 
-                        <img alt={stateItem} src={imageNote} />
-                        <img alt={stateItem} src={imageNote} />
-                        <img alt={stateItem} src={imageNote} />
-                        <img alt={stateItem} src={imageNote} />
-
-                    </div>
-
-
-                )
-
-            default:
-                alert('default')
-                break;
+    useEffect(() => {
+        const STATES =
+        {
+            Programacao: "Programação",
+            Arte: "Arte",
+            Hobby: "Hobby"
         }
-    }
+        function showInfo() {
+            switch (myTitle) {
+                case STATES.Programacao:
+                    
+                    return (
+                        <div>
+                            <h1>{myTitle}</h1>
+
+                            <img alt={myTitle} src={imageNote} />
+                            <p>sdsdsdsdds</p>
+                        </div>
+                    )
+                case STATES.Arte:
+                    break;
+                case STATES.Hobby:
+                    break;
+                default:                    
+                    break;
+            }
+        }
+        setTitles([STATES.Programacao, STATES.Arte, STATES.Hobby]);
+        setMyTitle(STATES.Programacao);
+        setInfoText(showInfo());
+
+    }, [myTitle]);
+
     return (
         <section className="pageContent">
             <div className="infoCard">
@@ -49,22 +57,22 @@ function Home() {
                         , desafiando o erro para evoluir a cada solução!"
                     </p>
                 </div>
-
-                <div className="selectCard">
-                    <h3>NAVEGUE</h3>
-                    <div className="selectTitle">
-                        {myTitles.map((item) => {
-                            return (
-                                <button key={item} onClick={() => setInfoText(showInfo(item))}>{item}</button>
-                            )
-                        })}                       
-                    </div>
-                    <div className="myCard">
-                            {infoText}
-                    </div>
-                </div>
-
             </div>
+            <div className="selectCard">
+                <h3>NAVEGUE</h3>
+                <div className="selectTitle">
+                    {myTitles.map((item) => {
+                        return (
+                            <button key={item} onClick={() => setMyTitle(item)}>{item}</button>
+                        )
+                    })}
+                </div>
+            </div>
+            <div className="selectedCardInfo">
+                {infoText}
+            </div>
+
+
         </section >
     )
 }
