1dcba90bb4418e49c5e4a688b9425a47fd80d6e6
artcodezen <henrique.watpequim@gmail.com>
2022-09-22 17:17:41 -0300

    novos arquivos art hobby

--- my-app/src/pages/Home/index.js
+++ my-app/src/pages/Home/index.js
@@ -5,6 +5,16 @@ import imgArduino from '../../assets/pictures/ImagesForSite/embedded.svg';
 import imgProg from '../../assets/pictures/ImagesForSite/frontend.svg';
 import imgLinux from '../../assets/pictures/ImagesForSite/linux.svg';
 import imgBackEnd from '../../assets/pictures/ImagesForSite/backend.svg';
+
+// art
+import imgArt from '../../assets/pictures/ImagesForSite/Art/art.webp';
+import certUdemy from '../../assets/pictures/ImagesForSite/Art/certificadoDesenho.webp';
+
+// hobby
+import imgHeli from '../../assets/pictures/ImagesForSite/Hobby/heli.webp';
+import imgDrone from '../../assets/pictures/ImagesForSite/Hobby/drone.webp';
+import imgAero from '../../assets/pictures/ImagesForSite/Hobby/aeromodel.svg';
+
 import { useEffect, useState } from "react";
 function Home() {
 
@@ -28,32 +38,56 @@ function Home() {
                     return (
                         <div>
                             <div className="cardInfoContent">
-                                <img alt={myTitle} src={imgArduino} />
+                                <img className="svg" alt={myTitle} src={imgArduino} />
                                 <p><b>Programação para microcontroladores</b> na
                                     linguagem c/c++, experiencia com firmware para drones e IOT</p>
                             </div>
                             <div className="cardInfoContent">
-                                <img alt={myTitle} src={imgLinux} />
+                                <img className="svg" alt={myTitle} src={imgLinux} />
                                 <p> <b>Sistema Linux</b>, Instalação, configuração Distros Debian e OpenSuse</p>
                             </div>
                             <div className="cardInfoContent">
-                                <img alt={myTitle} src={imgProg} />
+                                <img className="svg" alt={myTitle} src={imgProg} />
                                 <p> <b>Desenvolvimento de páginas web</b>, utilizando as tecnologias HTML5, CSS3, SASS, Javascript e React.JS</p>
                             </div>
                             <div className="cardInfoContent">
-                                <img alt={myTitle} src={imgBackEnd} />
+                                <img className="svg" alt={myTitle} src={imgBackEnd} />
                                 <p> <b>Desenvolvimento em backend</b>, utilizando as linguagens de programação C/C++, Python, Java</p>
                             </div>
                         </div>
                     )
                 case STATES.Arte:
                     return (
-                        <div></div>
+                        <div>
+                            <div className="cardInfoContent">
+                                <img alt={myTitle} src={imgArt} />
+                                <p> <b>Desenho realista de rostos</b>, representação de expressões de faces utilizando lápis, grafite,
+                                    caneta (ballpoint) e formato digital</p>
+                            </div>
+                            <div className="cardInfoContent">
+                                <img alt={myTitle} src={certUdemy} />
+                                <p> <b>Udemy certificado de desenho</b>, O desenho de cabeças, rostos e retratos</p>
+                            </div>
+                        </div>
+
                     );
 
                 case STATES.Hobby:
                     return (
-                        <div></div>
+                        <div>
+                             <div className="cardInfoContent">
+                                <img alt={myTitle} src={imgHeli} />
+                                <p> <b>Heli R/C</b>, Eletric / Glow; Montagem, configuração e voo em modo Sport / 3D acrobatico</p>
+                            </div>
+                            <div className="cardInfoContent">
+                                <img alt={myTitle} src={imgDrone} />
+                                <p> <b>Drones</b>, Montagem, configuração de embarcados, fabricação de circuitos PDB e frames</p>
+                            </div>
+                            <div className="cardInfoContent">
+                                <img alt={myTitle} src={imgAero} />
+                                <p> <b>Aeromodelismo</b>, Montagem, configuração, fabricação de fuselagem voo estilo acrobático</p>
+                            </div>
+                        </div>
                     );
 
                 default:
