1b6bc9767240be93de7ca4bbaf1c843df961096d
artcodezen <henrique.watpequim@gmail.com>
2022-10-12 14:07:16 -0300

    novo footer

--- my-app/src/components/Footer/index.js
+++ my-app/src/components/Footer/index.js
@@ -1,11 +1,30 @@
 import "./footer.css"
+import imgInstagram from "../../assets/footerImages/instagram.svg";
+import imgGithub from "../../assets/footerImages/github.svg";
+import imgTikTok from "../../assets/footerImages/tiktok.svg";
+import imgYoutube from "../../assets/footerImages/youtube.svg";
 
-function Footer(){
-    return(
+function Footer() {
+    const arrayPathImg = [imgInstagram, imgGithub, imgTikTok, imgYoutube];
+    const arrayUrl = ['https://www.instagram.com/artcodezen/', 'https://github.com/artcodezen',
+                        'https://www.tiktok.com/@artcodezen', 'https://www.youtube.com/channel/UCi3xYozMiKuvpc-aTbAgA3Q'];
+    let arrayItems = [];
+    for(let i = 0; i < arrayPathImg.length; i++){
+        let jsonitem = {};
+        jsonitem = {img: arrayPathImg[i], link: arrayUrl[i]};
+        arrayItems.push(jsonitem);
+    }
+    return (
         <footer className="footer">
-            <p>Powered by Henrique H. Watanabe&copy;</p>            
-            <div className="links">
+            <div className="author">
+                <p>Powered by Henrique H. Watanabe&copy;</p>
             </div>
+            <ul className='links'>
+                {arrayItems.map((item)=>{
+                    return (<li className='itemsList'><a href={item.link}><img src={item.img} alt="item.img"/></a></li>)
+                })}
+            </ul>
+           
         </footer>
     )
 }
